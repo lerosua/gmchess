c192b7f0f49fcd4209b435e2b9c5ee78fe1b24c7
lerosua <lerosua@5daa8cae-f998-11dd-a320-db7d8e7a638f>
2009-03-20 13:35:22 +0000

    fix po build
    git-svn-id: https://gmchess.googlecode.com/svn/trunk@83 5daa8cae-f998-11dd-a320-db7d8e7a638f

--- src/main.cc
+++ src/main.cc
@@ -20,9 +20,12 @@
 #include <libglademm/xml.h>
 #include <iostream>
 #include <gtkmm.h>
-//#include <gtkmm/window.h>
 #include <gtkmm/main.h>
+#include <glib/gi18n.h>
 #include "MainWindow.h"
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 
 #ifdef ENABLE_NLS
@@ -31,6 +34,11 @@
 
 int main (int argc, char *argv[])
 {
+
+	bindtextdomain (GETTEXT_PACKAGE, GMCHESS_LOCALEDIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
+
 	Gtk::Main kit(argc, argv);
 	MainWindow win;
 	//Board board;
